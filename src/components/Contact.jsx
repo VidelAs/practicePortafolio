0ada358534188f8fdb80a6a17772a8c9b1896d46
VidelAs <videl131103@gmail.com>
2023-07-13 15:25:42 -0400

    changes files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,15 @@
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#D0DEE1] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+    <div name='contact' className='w-full h-screen bg-[#D0DEE1] flex justify-center items-center'>
+        <form method='POST' action="https://getform.io/f/3aaa6a9b-adc0-4009-8368-21159e2aafa2" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#1281BD]  text-[#041658]'>Contact</p>
-                <p className=' text-[#5f6063] py-4'>// Submit the form below or shoot me an email - vutukuri.kumar192st.niituniversity.in</p>
+                <p className=' text-[#5f6063] py-4'>Submit the form below or shoot me an email - Videl131103@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-[#CCFFFF] border-2 hover:bg-[#041658] hover:border-[#041658] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <input className='rounded-lg bg-[#041658] text-[#D0DEE1] p-2' type="text" placeholder='Name' name='name' maxLength={30} />
+            <input className='rounded-lg my-4 p-2 bg-[#041658] text-[#D0DEE1]' type="email" placeholder='Email' name='email' />
+            <textarea className='rounded-lg bg-[#041658] text-[#D0DEE1] p-2' name="message" rows="10" placeholder='Message' maxLength={350}></textarea>
+            <button className='text-[#535459 ] border-2 hover:bg-[#1281BD] hover:border-[#1281BD] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://mail.google.com/mail/videl131103@gmail.com"
+              href="/"
             >
               Email <HiOutlineMail size={30} />
             </a>
